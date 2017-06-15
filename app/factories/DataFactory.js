c54220e128de78a0830e9d87678f2adafe72f556
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-06-14 19:34:48 -0500

    in process - making files and folders

---
+++ app/factories/DataFactory.js
@@ -0,0 +1,50 @@
+"use strict";
+
+app.factory('DataFactory', [function($q, $http){
+
+
+
+
+	const getSearchParams = () => {
+		return searchParams;
+	};
+
+
+	//
+	const setSearchParam = (obj) => {
+
+	};
+
+
+	//
+	const getFlights = (searchParams) => {
+
+	};
+
+
+	//
+	const getDestinations = (searchParams) => {
+
+	};
+
+
+	const getLocation = (originIATA, destinationIATA) => {
+
+	};
+
+
+	const getTripTime = (originObj, destinationObj) => {
+
+	};
+
+
+	//
+	const getLodging = (searchParams) => {
+
+	}
+
+
+
+
+
+}]);
