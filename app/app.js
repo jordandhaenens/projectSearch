b970a572ef99e2209a6925e5d69fb47edf805eb6
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-06-20 12:38:34 -0500

    authFactory is setup navBar has login and logout buttons

--- app/app.js
+++ app/app.js
@@ -3,13 +3,25 @@ console.log("App");
 
 const app = angular.module('Weekender', ['ngRoute']);
 
+let isAuth = (AuthFactory) => new Promise ( (resolve, reject) => {
+	AuthFactory.isAuthenticated()
+	.then( (userExists) => {
+		if (userExists){
+			console.log("Authenticated, go ahead");
+			resolve();
+		}else {
+			console.log("Not Authenticated");
+			reject();
+		}
+	});
+});
 
 
 app.config( ($routeProvider) => {
 	$routeProvider
 	.when('/', {
-		templateUrl: '',
-		controller: ''
+		templateUrl: 'partials/HomeView.html',
+		controller: 'HomeViewCtrl'
 	})
 	.when('/home', {
 		templateUrl: 'partials/HomeView.html',
@@ -23,5 +35,17 @@ app.config( ($routeProvider) => {
 		templateUrl: 'partials/SelectedTrip.html',
 		controller: 'SelectedTripCtrl'
 	})
-	.otherwise('/');
+	.otherwise('/home');
+});
+
+
+app.run(($location, FBCreds) => {
+	let creds = FBCreds;
+	let authConfig = {
+		apiKey: creds.apiKey,
+		authDomain: creds.authDomain,
+		databaseURL: creds.databaseURL
+	};
+
+	firebase.initializeApp(authConfig);
 });
