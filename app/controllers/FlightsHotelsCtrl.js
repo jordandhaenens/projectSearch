4a2bd32eee87234778ed94391e94c4cb1a052818
Jordan Dhaenens <jordan.dhaenens@gmail.com>
2017-07-05 11:51:35 -0500

    cleaning it up

--- app/controllers/FlightsHotelsCtrl.js
+++ app/controllers/FlightsHotelsCtrl.js
@@ -6,52 +6,38 @@ app.controller('FlightsHotelsCtrl', ['$timeout', 'API', '$scope', 'DataFactory',
 
 
 	$scope.params = DataFactory.searchParams;
-	console.log("searchParams in FlightsHotelsCtrl", $scope.params);
 	$scope.hideHotels = true;
-	// $scope.hideFlights = '';
 	$scope.hideFlights = false;
 
 	$scope.data = {
 		selected: null,
-		// flightError: false,
 		flights: [],
 		lodging: []
 	};
 
 
-	// let goHome = function(){
-	// 	console.log("goHome fired");
-	// 	$location.path('/home');
-	// };
-
 	let getFlights = function(){
-		console.log("$scope.params on when getFlights()", $scope.params);
 		API.getFlights($scope.params)
-		// API.getFlights(testParams)
 		.then( (response) => {
 			$scope.data.flights = response;
-			console.log("flattenedflights in FlightsViewCtrl", $scope.data.flights);
+			// console.log("flattenedflights in FlightsViewCtrl", $scope.data.flights);
 		})
 		.catch(function(error){
-			console.log(error);
 			$scope.data.flightError = true;
 			$scope.hideFlights = true;
 			$timeout(function(){
-				console.log("goHome fired");
 				$location.path('/home');
 			}, 4500);
 		});
 	};
 
 	$scope.getLodging = function() {
-		// $scope.hideFlights = $scope.params.lodging;
 		$scope.hideFlights = true;
-		// $scope.params.lodging = false;
 		$scope.params.lodgingSearch = false;
 		API.getLodging($scope.params)
 		.then( (response) => {
 			$scope.hideHotels = false;
-			console.log("getLodging response", response);
+			// console.log("getLodging response", response);
 			//filter this by response.lodgingPrice not to exceed $scope.params.lodgingPriceCap
 			$scope.data.lodging = response;
 		})
@@ -65,19 +51,18 @@ app.controller('FlightsHotelsCtrl', ['$timeout', 'API', '$scope', 'DataFactory',
 	$scope.updateParams = function(obj) { //obj is the selected flight or hotel from the partial
 		// let troll = "$$hashKey";
 		Object.keys(obj).forEach(function(keyVal){
-		console.log('keyVal added to searchParams', keyVal);
+		// console.log('keyVal added to searchParams', keyVal);
 			// if (keyVal !== troll) {
 				$scope.params[keyVal] = obj[keyVal];
 			// }
 		});
-		console.log("updated $scope.params", $scope.params);
+		// console.log("updated $scope.params", $scope.params);
 	};
+
 
 	$scope.selectedTripView = function() {
 		$location.path('/selectedTripView');
-
 	};
-
 
 
 	getFlights();
--- app/controllers/HomeViewCtrl.js
+++ app/controllers/HomeViewCtrl.js
@@ -5,7 +5,7 @@ app.controller('HomeViewCtrl', ['$location', '$timeout', 'API', '$scope', 'DataF
 
 	$scope.switch = true;
 	$scope.params = DataFactory.searchParams;
-	console.log("searchParams in HomeViewCtrl", $scope.params);
+	// console.log("searchParams in HomeViewCtrl", $scope.params);
 
 	$scope.data = {
 		selected: null,
@@ -23,14 +23,15 @@ app.controller('HomeViewCtrl', ['$location', '$timeout', 'API', '$scope', 'DataF
 		}
 	};
 
+
 	$scope.getDestinations = function(){
 		let r = moment($scope.params.retDate, 'MMM-DD-YYYY').format('YYYY-MM-DD');
 		let d = moment($scope.params.depDate, 'MMM-DD-YYYY').format('YYYY-MM-DD');
 		$scope.params.retDate = r;
 		$scope.params.depDate = d;
 		duration($scope.params.retDate, $scope.params.depDate);
-		console.log("lodgingOpt", $scope.params.lodgingOpt);
-		if ($scope.params.lodgingOpt !== ""){ //should this be equals instead?
+		// console.log("lodgingOpt", $scope.params.lodgingOpt);
+		if ($scope.params.lodgingOpt !== ""){
 			$scope.params.lodging = true;
 		}
 		$scope.switch = false;
@@ -38,8 +39,8 @@ app.controller('HomeViewCtrl', ['$location', '$timeout', 'API', '$scope', 'DataF
 		.then( (response) => {
 			$scope.data.fullData = response;
 			$scope.data.flights = response.results;
-			console.log("$scope.data", $scope.data.fullData);
-			console.log("$scope.flights", $scope.data.flights);
+			// console.log("$scope.data", $scope.data.fullData);
+			// console.log("$scope.flights", $scope.data.flights);
 		})
 		.catch(function(){
 			$scope.data.flightError = true;
@@ -52,9 +53,6 @@ app.controller('HomeViewCtrl', ['$location', '$timeout', 'API', '$scope', 'DataF
 	};
 
 
-
-	//Subtract retDate date obj from depDate date obj to get duration and add to $scope.params
-	// $scope.params.duration = duration($scope.params.retDate, $scope.params.depDate);
 	let duration = function(retDate, depDate){
 		let dep = moment(new Date(depDate), 'yyyy-mm-dd');
 		let ret = moment(new Date(retDate), 'yyyy-mm-dd');
@@ -64,45 +62,6 @@ app.controller('HomeViewCtrl', ['$location', '$timeout', 'API', '$scope', 'DataF
 		//add days to params
 		$scope.params.totalDays = days;
 	};
-
-	//This is for the date-picker
-	// $scope.currentTime = new Date();
-	// // $scope.departDate = $scope.params.depDate;
-	// // $scope.returnDate = $scope.params.retDate;
-	// $scope.month = ['Januar', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	// $scope.monthShort = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-	// $scope.weekdaysFull = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	// $scope.weekdaysLetter = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-	// $scope.disable = [false];
-	// $scope.today = 'Today';
-	// $scope.clear = 'Clear';
-	// $scope.close = 'Close';
-	// var days = 300;
-	// $scope.minDate = (new Date($scope.currentTime.getTime())).toISOString();
-	// $scope.maxDate = (new Date($scope.currentTime.getTime() + ( 1000 * 60 * 60 *24 * days ))).toISOString();
-	// $scope.onStart = function () {
-	//     console.log('onStart');
-	// };
-	// $scope.onRender = function () {
-	//     console.log('onRender');
-	// };
-	// $scope.onOpen = function () {
-	//     console.log('onOpen');
-	// };
-	// $scope.onClose = function () {
-	//     console.log('onClose');
-	//     console.log('$scope.params.depDate', $scope.params.depDate);
-	//     console.log('$scope.params.retDate', $scope.params.retDate);
-	// };
-	// $scope.onSet = function () {
-	//     console.log('onSet');
-	// };
-	// $scope.onStop = function () {
-	//     console.log('onStop');
-	// };
-
-
-
 
 
 
